1f2fe2ee86b08727cb3f7d5245a39acd93828a41
yikhong <frenz_band@live.com>
2020-08-23 13:41:19 +0800

    add git clone

--- capture_link.py
+++ capture_link.py
@@ -28,6 +28,7 @@ for index, channel in enumerate(tv_channel):
 
 # os.chdir("helloworld")
 # os.system("git pull")
+git_change = False
 with open("testing_1.m3u", 'w', encoding="utf-8") as output_testing:
     with open("testing.m3u", encoding="utf-8") as git_file:
         for line in git_file:
@@ -40,6 +41,7 @@ with open("testing_1.m3u", 'w', encoding="utf-8") as output_testing:
                 if match_obj.group(1) != ch_sec_group:
                     print("update new link for NTV7")
                     line = re.sub(r'.*\.m3u8', ch_addr['NTV7'], line)
+                    git_change = True
             if re.match(r'.*(sec\(.*\))/dm.*kdFzSYy1bHxrForBrar$', line):
                 match_obj = re.match(r'.*(sec\(.*\))/dm.*kdFzSYy1bHxrForBrar$', line)
                 print(line)
@@ -48,9 +50,15 @@ with open("testing_1.m3u", 'w', encoding="utf-8") as output_testing:
                 if match_obj.group(1) != ch_sec_group:
                     print("update new link for TV8")
                     line = re.sub(r'.*\.m3u8', ch_addr['TV8'], line)
+                    git_change = True
 
             output_testing.write(line)
 
 os.remove("testing.m3u")
 os.rename("testing_1.m3u", "testing.m3u")
+
+if git_change:
+    os.makedirs("git_hub_commit")
+    os.chdir("git_hub_commit")
+    os.system("git clone https://github.com/yikhong92/helloworld.git .")
 
